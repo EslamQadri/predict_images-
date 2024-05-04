3f6f7e87804161b06eec19434113bdc3fbf61403
EslamQadri <eslamkadryuwk@gmail.com>
2024-05-05 01:48:10 +0300

    add main page

--- egy_lens/urls.py
+++ egy_lens/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from egy_lens.views import process_image
+from egy_lens.views import process_image, index
 
 urlpatterns = [
     path("api/process-image/", process_image, name="process_image"),
+    path("", index, name="index"),
 ]
--- egy_lens/views.py
+++ egy_lens/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from egy_lens.funcatoins import predict_and_detect
 from ultralytics import YOLO
 import os
@@ -18,7 +18,7 @@ def process_image(request):
         # Assuming the image is sent as a form field named 'image'
         uploaded_image = request.FILES.get("image")
         if uploaded_image:
-          
+
             image_data = (
                 uploaded_image.read()
             )  # Read the image data from the file object
@@ -38,9 +38,13 @@ def process_image(request):
                     "Predicted_Labels": "thing",
                 }
             ]
-            processed_data = {"result":predicted_labels[0] }
+            processed_data = {"result": predicted_labels[0]}
             return JsonResponse(processed_data)
         else:
             return JsonResponse({"error": "No image uploaded"}, status=400)
     else:
         return JsonResponse({"error": "Method not allowed"}, status=405)
+
+
+def index(request):
+    return HttpResponse("hi")
